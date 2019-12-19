7bb07ee1ac7d174a57394e68d7d17d315bd22e36
mgtoxd <51752260+mgtoxd@users.noreply.github.com>
2019-12-19 18:25:32 +0800

    2019-12-19 Quickstart-2增加注释

--- 1-shiro/src/main/java/pers/mtx/Quickstart.java
+++ 1-shiro/src/main/java/pers/mtx/Quickstart.java
@@ -30,6 +30,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Simple Quickstart application showing how to use Shiro's API.
+ * 一个简单的Quickstart应用程序，显示了如何使用Shiro的API。
  *
  * @since 0.9 RC2
  */
@@ -47,6 +48,12 @@ public class Quickstart {
 
         // Use the shiro.ini file at the root of the classpath
         // (file: and url: prefixes load from files and urls respectively):
+        /*
+        创建带有已配置的领域，用户，
+        角色和权限的Shiro SecurityManager的最简单方法是使用简单的INI配置。
+         //我们将使用一个可以摄取.ini文件并//返回SecurityManager实例的工厂来做到这一点：
+         //在类路径的根目录下使用shiro.ini文件//（file：和url：前缀加载分别来自文件和网址）：
+         */
         Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
         SecurityManager securityManager = factory.getInstance();
 
@@ -56,32 +63,53 @@ public class Quickstart {
         // webapps.  That is outside the scope of this simple quickstart, so
         // we'll just do the bare minimum so you can continue to get a feel
         // for things.
+        /*
+        对于这个简单的示例快速入门，使SecurityManager
+         //作为JVM单例进行访问。大多数应用程序不这样做//，
+         而是依靠其容器配置或web.xml来// webapps。这超出了此简单快速入门的范围，
+         因此//我们将只做最少的工作，以便您可以继续对事物有所了解。
+         */
         SecurityUtils.setSecurityManager(securityManager);
 
         // Now that a simple Shiro environment is set up, let's see what you can do:
-
+        //现在已经建立了一个简单的Shiro环境，让我们看看您可以做什么：
         // get the currently executing user:
+        /*
+        获取当前执行的用户：
+         */
         Subject currentUser = SecurityUtils.getSubject();
 
         // Do some stuff with a Session (no need for a web or EJB container!!!)
+        /*
+        使用Session做一些事情（不需要Web或EJB容器！！！）
+         */
         Session session = currentUser.getSession();
-        session.setAttribute("someKey", "aValue");
+        /*
+        添加session
+         */
+        session.setAttribute("someKey", "aValue"); //这个session是apache的session,不是servlet那个
+        /*
+        取值
+         */
         String value = (String) session.getAttribute("someKey");
         if (value.equals("aValue")) {
             log.info("Retrieved the correct value! [" + value + "]");
         }
 
         // let's login the current user so we can check against roles and permissions:
-        if (!currentUser.isAuthenticated()) {
+        /*
+        让我们登录当前用户，以便我们可以检查角色和权限：
+         */
+        if (!currentUser.isAuthenticated()) {//检查是否认证
             UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
             token.setRememberMe(true);
             try {
                 currentUser.login(token);
-            } catch (UnknownAccountException uae) {
+            } catch (UnknownAccountException uae) {//未知帐户异常,没有该账户
                 log.info("There is no user with username of " + token.getPrincipal());
             } catch (IncorrectCredentialsException ice) {
                 log.info("Password for account " + token.getPrincipal() + " was incorrect!");
-            } catch (LockedAccountException lae) {
+            } catch (LockedAccountException lae) {//锁定帐户例外
                 log.info("The account for username " + token.getPrincipal() + " is locked.  " +
                         "Please contact your administrator to unlock it.");
             }
