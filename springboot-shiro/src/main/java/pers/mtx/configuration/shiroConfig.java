eec30ed03c2d556e9df2d52381d378ed652db372
mgtoxd <51752260+mgtoxd@users.noreply.github.com>
2019-12-23 18:21:35 +0800

    2019-12-23 thymeleaf配合shiro初步完成

--- springboot-shiro/src/main/java/pers/mtx/configuration/shiroConfig.java
+++ springboot-shiro/src/main/java/pers/mtx/configuration/shiroConfig.java
@@ -1,5 +1,6 @@
 package pers.mtx.configuration;
 
+import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -77,5 +78,14 @@ public class shiroConfig {
     @Bean(name = "userRealm")
     public UserRealm getRealm(){
         return new UserRealm();
+    }
+
+
+    /*
+    配置ShiroDialect,用于thymeleaf和shiro标签配合使用
+     */
+    @Bean
+    public ShiroDialect getShiroDialect(){
+        return new ShiroDialect();
     }
 }
